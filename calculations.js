91e7059042efa3d92a3208ece42ca9422177c432
OksanaTarun <76516037+OksanaTarun@users.noreply.github.com>
2022-11-01 18:58:32 +0300

    added inc dec

--- calculations.js
+++ calculations.js
@@ -22,3 +22,19 @@ console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
 console.log(divideBalanceByAccounts(3000, 2)); 
 console.log(getRestAfterDivision(7000, 3));
+
+function increment(x, y) {
+  x = x - y;
+  y = ++x;
+  return y;
+}
+
+console.log(increment(10, 6));
+
+function decrement(x, y) {
+  x = x - y;
+  y = --x;
+  return y;
+}
+
+console.log(decrement(10, 6));
