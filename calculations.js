c904dcd109c293135725ea72c2addae4a3d93485
OksanaTarun <76516037+OksanaTarun@users.noreply.github.com>
2022-11-01 18:03:48 +0300

    used assignment operators

--- calculations.js
+++ calculations.js
@@ -1,26 +1,24 @@
 function increaseBalance(x, y) {
-  x = x + y;
+  x += y;
   return x;
 }
 
-//не передана переменная y
 function decreaseBalance(x, y) {
-x = x - y;
-return x;
+ x -= y;
+ return x;
 }
 
-//удалили недостижимый код, вызываем ниже
 function divideBalanceByAccounts(x, y) {
-  x = x / y;
+  x /= y;
   return x;
 }
 
 function getRestAfterDivision(x, y) {
-  x = x % y;
+  x %= y;
   return x;
 }
 
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
-console.log(divideBalanceByAccounts(3000, 2)); // лишнее значение 4000, передали только 2 параметра x и y
+console.log(divideBalanceByAccounts(3000, 2)); 
 console.log(getRestAfterDivision(7000, 3));
